cc4734548572c3e52b8810686450a4b8e0e4dfb3
mo-amaan <mo.amaan215@gmail.com>
2025-03-03 20:55:26 -0400

    Server changes added basic html

--- src/server.js
+++ src/server.js
@@ -1,15 +1,27 @@
 import express from "express";
-import auMiddleware from "./middlewares/auMiddleware.js";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 import auRoutes from "./routes/auRoutes.js";
 import todoRoutes from "./routes/todoRoutes.js";
+import auMiddleware from "./middlewares/auMiddleware.js";
+
 const app = express();
-const port = process.env.PORT || 4005;
+const PORT = process.env.PORT || 4004;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "../public")));
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// Routes
 app.use("/auth", auRoutes);
 app.use("/todos", auMiddleware, todoRoutes);
 
-app.listen(port, () => {
-  console.log(`Server active on port: ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server has started on port: ${PORT}`);
 });
