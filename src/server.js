4b6d415cdc70164a401104602bc6c74f1993a733
mo-amaan <mo.amaan215@gmail.com>
2025-03-03 20:42:54 -0400

    todo routs , docker and prisma set up

--- src/server.js
+++ src/server.js
@@ -1,9 +1,14 @@
 import express from "express";
-
+import auMiddleware from "./middlewares/auMiddleware.js";
+import auRoutes from "./routes/auRoutes.js";
+import todoRoutes from "./routes/todoRoutes.js";
 const app = express();
-const port = process.env.PORT() || 4005;
+const port = process.env.PORT || 4005;
 
 app.use(express.json());
+
+app.use("/auth", auRoutes);
+app.use("/todos", auMiddleware, todoRoutes);
 
 app.listen(port, () => {
   console.log(`Server active on port: ${port}`);
